9341ac9b3f49992b141729cfe99687a6b1298d4c
Dillon Arnold <djarnold@wpi.edu>
2020-05-09 14:49:24 -0400

    Changed products page

--- src/routes/products.js
+++ src/routes/products.js
@@ -8,7 +8,7 @@ const Products = () => (
       <div class="product">
         <img alt="Product 1" src="../images/watch-1.jpg" />
         <h2>Product 1</h2>
-        <p>Product 1 description</p>
+        <p>none</p>
       </div>
       <div class="product">
         <img alt="Product 2" src="../images/watch-2.jpg" />
